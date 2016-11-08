62f3c54ab8f3279fafa827fce42924a52e7a9698
phil olson <philip.olson@protonmail.ch>
2016-11-07 22:34:23 -0600

    setting repo up for travis

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
-. "$(locate nvm.sh)"
-
-./build.sh \
-  && nvm run 6.0.0 ./node_modules/mocha/bin/mocha --color=always tests/es6.js \
-  && nvm run 0.10.0 ./node_modules/mocha/bin/mocha --color=always tests/es5.js
-
-nvm use node
+# shellcheck disable=2086
+if [ -z ${TRAVIS_NODE_VERSION+x} ]; then
+  # shellcheck disable=1090
+  . "$(locate nvm.sh | head -n1)"
+  ./build.sh \
+    && nvm run 6.0.0 ./node_modules/mocha/bin/mocha --color=always tests/es6.js \
+    && nvm run 0.10.0 ./node_modules/mocha/bin/mocha --color=always tests/es5.js
+elif [ "${TRAVIS_NODE_VERSION}" = "6.0.0" ]; then
+  ./node_modules/mocha/bin/mocha --color=always tests/es6.js
+else # TRAVIS_NODE_VERSION < 6
+  ./build.sh \
+    && ./node_modules/mocha/bin/mocha --color=always tests/es5.js
+fi
